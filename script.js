2ddc76d3d3c8b4ed31947185f7337be2d570b9e5
Lynn Chaker <lchaker921@tamu.edu>
2026-02-19 16:59:21 +0000

    Math functions in JavaScript

---
+++ script.js
@@ -0,0 +1,16 @@
+function addNumbers(a, b) {
+  return a + b;
+
+}
+
+function multiplyNumbers(a, b) {
+  return a * b;
+}
+
+console.log(addNumbers(5, 10)); // Output: 15
+console.log(addNumbers(3, 7)); // Output: 10
+console.log(addNumbers(-2, 4)); // Output: 2
+
+console.log(multiplyNumbers(5, 10)); // Output: 50
+console.log(multiplyNumbers(3, 7)); // Output: 21
+console.log(multiplyNumbers(-2, 4)); // Output: -8
